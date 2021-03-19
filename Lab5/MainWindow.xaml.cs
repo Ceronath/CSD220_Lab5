229d7d2026ea46c519a14cbbd44fabd078ea1b64
Ceronath <jordan_poulin10@hotmail.com>
2021-03-18 20:09:26 -0400

    rough commit of GUI design

---
+++ Lab5/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string connectionString = "server=localhost;user id=root;database=superheros";
+
+        MySqlConnection conn = new MySqlConnection(connectionString);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
